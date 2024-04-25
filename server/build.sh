4b2b2ed9d6937979545aa98e787154a578d6e8c2
DeanCode <ds@xxxmail.eu>
2024-04-25 10:55:58 +0200

    Fixed relative paths

--- server/build.sh
+++ server/build.sh
@@ -6,4 +6,8 @@ source patstrap-server-env/bin/activate
 pip install -r requirements.txt
 
 # Use pyinstaller to create a standalone LINUX executable at ./dist/patstrap-server/patstrap-server
-pyinstaller gui.py -n patstrap-server --noconfirm --path=./patstrap-server-env/lib/python3.11/site-packages --hidden-import zeroconf._utils.ipaddress --hidden-import zeroconf._handlers.answers
+pyinstaller gui.py -n patstrap-server --noconfirm --onedir \
+	--path=./patstrap-server-env/lib/python3.11/site-packages \
+	--add-data "./global.css:." \
+	--hidden-import zeroconf._utils.ipaddress \
+	--hidden-import zeroconf._handlers.answers
